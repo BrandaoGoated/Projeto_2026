1e0bee8ae444aee419ff257a072a52257c09d7f1
ozefreitas <jpsfreitas12@gmail.com>
2026-02-04 13:16:17 +0000

    [UPDATE] dev environment

--- ci/ci_build.sh
+++ ci/ci_build.sh
@@ -1,9 +1,14 @@
 PREFIX="/opt/conda"
+SRC="/app"
+
 mkdir -p "${PREFIX}/bin"
-cp M-PARTY/m_party.py "${PREFIX}/bin/"
-cp -r M-PARTY/workflow "${PREFIX}/bin/"
-# cp -r M-PARTY/workflow/scripts "${PREFIX}/bin/"
-cp -r M-PARTY/resources "${PREFIX}/bin"
-# cp -r M-PARTY/results "${PREFIX}/bin"
-cp -r M-PARTY/config "${PREFIX}/bin"
-chmod +x /opt/conda/bin/m_party.py
+
+cp "${SRC}/m_party.py" "${PREFIX}/bin/"
+cp -r "${SRC}/workflow" "${PREFIX}/bin/"
+cp -r "${SRC}/resources" "${PREFIX}/bin/"
+cp -r "${SRC}/config" "${PREFIX}/bin/"
+
+chmod +x "${PREFIX}/bin/m_party.py"
+
+# alias
+ln -s /opt/conda/bin/m_party.py /opt/conda/bin/m-party
